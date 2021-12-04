32fc90d01f4ad573524a4c3d176ff5d96a0fe21e
Alex Ehrenhöfer <alexander.ehrenhoefer@student.htw-berlin.de>
2021-12-04 18:39:25 +0100

    implemented Queen Blocking Methods

--- src/ChessController.java
+++ src/ChessController.java
@@ -1,6 +1,32 @@
 public class ChessController {
+    Chessboard board;
+
     public static void main(String[] args) {
-        Chessboard board = new Chessboard();
+        ChessController cs = new ChessController();
+        cs.start();
+    }
+
+    public void start () {
+        board = new Chessboard();
         board.print();
+        setQueen(3,3);
+        board.print();
+    }
+
+    /**
+     * Sets a queen to the chess board and blocks all reachable fields
+     * @param x
+     * @param y
+     */
+    public void setQueen(int x, int y) {
+        // Set the queen
+        if (board.get(x, y) == 0) {
+            board.setQueen(x, y);
+        } else {
+            System.out.println("Field " + x + " " + y + " is already blocked");
+        }
+
+        // Block all reachable fields
+        board.blockFromField(x, y);
     }
 }
--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,4 +1,8 @@
 public class Chessboard {
+    public static int QUEEN = 9;
+    public static int BLOCKED = 1;
+    public static int EMPTY = 0;
+
     int width = 8;
     int height = 8;
 
@@ -8,14 +12,49 @@ public class Chessboard {
         for (int x = 0; x < width; x++) {
             board[x] = new int[height];
             for (int y = 0; y < height; y++) {
-                board[x][y] = 0;
+                board[x][y] = Chessboard.EMPTY;
             }
         }
     }
 
+    public int get(int x, int y) {
+        return board[x][y];
+    }
+
+    private void set(int x, int y, int value) {
+        board[x][y] = value;
+    }
+
+    public void setQueen(int x, int y) {
+        set(x, y, Chessboard.QUEEN);
+    }
+
+    /**
+     * Blocks the row and the diagonals from a given field
+     * @param x
+     * @param y
+     */
+    public void blockFromField(int x, int y) {
+        // block row
+        for (int xi = (x+1); xi < width; xi++) {
+            board[xi][y] = Chessboard.BLOCKED;
+        }
+
+        // block diagonal up
+        int yUp = y-1;
+        int yDown = y+1;
+        for (int xi = (x+1); xi < width; xi++) {
+            if (yUp >= 0) board[xi][yUp] = Chessboard.BLOCKED;
+            if (yDown < height) board[xi][yDown] = Chessboard.BLOCKED;
+            yUp--;
+            yDown++;
+        }
+    }
+
     public void print() {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+        System.out.println("--------board---------");
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 System.out.print(board[x][y] + "  ");
             }
             System.out.println();
